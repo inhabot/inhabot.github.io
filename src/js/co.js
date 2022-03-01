05753a6e376929d233dcb2e2e1da9e46cab67502
inhabot <reduction59@gmail.com>
2022-03-01 11:59:37 +0900

    kakao 오류

--- src/js/co.js
+++ src/js/co.js
@@ -246,49 +246,15 @@ $('.wrap').scroll(function () {
 });
 
 function sendLinkCustom() {
-  Kakao.init("4c199b94451dab5dd3c0b3060fd1abaf");
-  Kakao.Link.sendCustom({
-      templateId: 72115
-  });
-}
+  try {
+    if (!Kakao.isInitialized()) {
+      Kakao.init('4c199b94451dab5dd3c0b3060fd1abaf');
+    }
 
-// try {
-//   function sendLinkDefault() {
-//     Kakao.init('[Javascript API key]')
-//     Kakao.Link.sendDefault({
-//       objectType: 'feed',
-//       content: {
-//         title: '딸기 치즈 케익',
-//         description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
-//         imageUrl:
-//           'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-//         link: {
-//           mobileWebUrl: 'https://developers.kakao.com',
-//           webUrl: 'https://developers.kakao.com',
-//         },
-//       },
-//       social: {
-//         likeCount: 286,
-//         commentCount: 45,
-//         sharedCount: 845,
-//       },
-//       buttons: [
-//         {
-//           title: '웹으로 보기',
-//           link: {
-//             mobileWebUrl: 'https://developers.kakao.com',
-//             webUrl: 'https://developers.kakao.com',
-//           },
-//         },
-//         {
-//           title: '앱으로 보기',
-//           link: {
-//             mobileWebUrl: 'https://developers.kakao.com',
-//             webUrl: 'https://developers.kakao.com',
-//           },
-//         },
-//       ],
-//     })
-//   }
-// ; window.kakaoDemoCallback && window.kakaoDemoCallback() }
-// catch(e) { window.kakaoDemoException && window.kakaoDemoException(e) }
+    if (Kakao) {
+      Kakao.Link.sendCustom({
+        templateId: 72115
+      });
+    };
+  } catch (e) {};
+}
